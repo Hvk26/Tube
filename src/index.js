0ef08c9664e3d42a307413331b4aeb0bbaf92b2d
Hvk26 <hvk8789@gmail.com>
2024-05-25 20:36:18 +0530

    adding express and cookie parser configuration

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
 import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
 const app = express();
 
+
+//dotenv configuration
 dotenv.config({
     path: "./env"
-})
+});
+
+//CORS configuration (app.use is used for all kind of configuration in express)
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+
+//express configuration
+app.use(express.json({limit: "16kb"})); // to limit the amount of JSON request to server
+app.use(express.urlencoded({extended: true, limit:"16kb"})); // to make the server request from differnt URLs uniform
+app.use(express.static("public")); // to store some data from users into local public folder
+
+//cookie-parser configuration
+app.use(cookieParser());
 
 connectDB()
+//promise verification for debugging purpose
 .then(()=>{
 
     app.on("error", ()=>{
