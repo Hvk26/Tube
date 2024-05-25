bdf4c22605c03e7dfc6fd9e83c01e794c942ce43
Hvk26 <hvk8789@gmail.com>
2024-05-26 00:37:16 +0530

    updating the asynchandler function with a new asynchandler using Promise

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,12 +1,25 @@
-const asyncHandler = (requestHandler) => async(req, res, next) => {
-    try {
-        await requestHandler(req, res, next);
-    } catch (error) {
-        res.status(error.code || 300).json({
-            success: false,
-            message: error.message
-        });
-    }
-}
+const asyncHandler = (requestHandler)=>{(req, res, next)=>{
+    Promise.resolve(requestHandler(req, res, next)).catch((error)=>next(error))
+}}
+
+
+
+
+
+
+
+// adding the asynchandler utilities with async as an higher order function where request, response, and next(it is used for the middleware)
+//it is a type of method to utilise and resolve the request for the database
+
+// const asyncHandler = (requestHandler) => async(req, res, next) => {
+//     try {
+//         await requestHandler(req, res, next);
+//     } catch (error) {
+//         res.status(error.code || 300).json({
+//             success: false,
+//             message: error.message
+//         });
+//     }
+// }
 
 export {asyncHandler};
