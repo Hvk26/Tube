0e2d9748a3553c7c08cb6c19217e07ffa0d5a3af
Hvk26 <hvk8789@gmail.com>
2024-05-26 00:31:09 +0530

    adding an asynchandler utilities for the database connectivity

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use(cors({
 
 //express configuration
 app.use(express.json({limit: "16kb"})); // to limit the amount of JSON request to server
-app.use(express.urlencoded({extended: true, limit:"16kb"})); // to make the server request from differnt URLs uniform
+app.use(express.urlencoded({extended: true, limit:"16kb"})); // to make the server request from differnt URLs uniform (extended means that we can add objects inside of objects)
 app.use(express.static("public")); // to store some data from users into local public folder
 
 //cookie-parser configuration
