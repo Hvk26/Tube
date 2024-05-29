9658f5031a7f6f80cb894f41be4c78266cc6ad9b
Hvk26 <hvk8789@gmail.com>
2024-05-29 21:41:02 +0530

    adding cloudinary to handle fileupload

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,8 @@
+import fs from "fs"
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name: 'drrpkpcc5', 
+  api_key: '143282216962225', 
+  api_secret: '50tNM-AzL0G296d_BBLhRvqGm9o' 
+});
