1b5452051c90dc80a7d93376fa1e13d74bbfa367
Hvk26 <hvk8789@gmail.com>
2024-05-31 23:39:08 +0530

    added all cors and all config commands in app.js

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser" // cookie-parser is a kind of middleware it is used to get certain kind of permission
+
+const app = express();
+
+//CORS configuration (app.use is used for all kind of configuration in express)
+app.use(cors({
+    origin: process.env.CORS_ORIGIN, // this is the origin link that is taken from environment variables
+    credentials: true // this is to make the credentials true
+}));
+
+//express configuration
+app.use(express.json({limit: "16kb"})); // to limit the amount of JSON request to server
+app.use(express.urlencoded({extended: true, limit:"16kb"})); // to make the server request from differnt URLs uniform (extended means that we can add objects inside of objects)
+app.use(express.static("public")); // to store some data from users into local public folder
+
+//cookie-parser configuration
+app.use(cookieParser());
