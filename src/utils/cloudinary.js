27be5e0bfd622a0b329fe7097cad90d5bfaeb878
Hvk26 <hvk8789@gmail.com>
2024-06-02 21:07:37 +0530

    added unlink method synchronously to remove file from local machine

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -11,7 +11,8 @@ const uploadOnCloudinary = async (localFilePath)=>{
     try {
         if(!localFilePath) return null;
         const response = await cloudinary.uploader.upload(localFilePath, {resource_type: "auto"})
-        console.log("file has been succesfully uploaded", response.url);
+        // console.log("file has been succesfully uploaded", response.url);
+        fs.unlinkSync(localFilePath);
         return response;
     } catch (error) {
         fs.unlinkSync(localFilePath);
