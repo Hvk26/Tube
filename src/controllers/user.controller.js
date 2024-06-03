957980106d44514c53e36af4f20e7abc0d3189e9
Hvk26 <hvk8789@gmail.com>
2024-06-03 22:30:23 +0530

    creating a login user skeleton in user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -80,4 +80,11 @@ const registerUser = asyncHandler(async (req, res)=>{
     );
 });
 
-export {registerUser};
+const loginUser = asyncHandler(async (req, res)=> {
+
+});
+
+export {
+    registerUser,
+    loginUser
+};
