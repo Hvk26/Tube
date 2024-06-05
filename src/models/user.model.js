23764e4831520a444009b5714179514f944c9de1
Hvk26 <hvk8789@gmail.com>
2024-06-05 23:15:41 +0530

    formating the codebase

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -49,7 +49,7 @@ userSchema.pre("save", async function (next){
     if(!this.isModified("password")) return next();
     this.password = await bcrypt.hash(this.password, 10)
     next();
-})
+});
 
 userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password);
@@ -70,7 +70,7 @@ userSchema.methods.generateAccessToken = function(){
         }
 )
 
-}
+};
 
 userSchema.methods.generateRefreshToken = function(){
     return jwt.sign(
@@ -83,6 +83,6 @@ userSchema.methods.generateRefreshToken = function(){
         }
 )
 
-}
+};
 
 export const User = mongoose.model("User", userSchema);
