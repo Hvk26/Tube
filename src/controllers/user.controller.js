f4d413220b0d9429a866b00ec6d3e43f145b0919
Hvk26 <hvk8789@gmail.com>
2024-06-07 13:17:05 +0530

    adding verification to password check and returning response

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -210,7 +210,11 @@ const refreshAccessToken = asyncHandler(async (req, res)=> {
 
 const changeCurrentPassword = asyncHandler(async (req, res) => {
 
-    const {oldPassword, newPassword} = req.body;
+    const {oldPassword, newPassword, confirmPassword} = req.body;
+
+    if(!(oldPassword === confirmPassword)){
+        throw new ApiError(400, "Password does not match");
+    }
 
     const user =  await User.findById(user.req?._id);
 
@@ -219,6 +223,13 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
     if(!isPasswordCorrect){
         throw new ApiError(400, "Password invalid");
     }
+
+    user.password = newPassword;
+    await user.save({validateBeforeSave: false});
+
+    return res
+           .status(200)
+           .json(new ApiResponse(200, {}, "Password changed succesfully"));
 })
 export {
     registerUser,
