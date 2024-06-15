d82f7289042c5e08b6de22a5322c02f8c6c9cca7
Hvk26 <hvk8789@gmail.com>
2024-06-15 13:34:22 +0530

    creating a publishAVideo method to publish a video

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -9,20 +9,56 @@ import {uploadOnCloudinary} from "../utils/cloudinary.js"
 
 
 const getAllVideos = asyncHandler(async (req, res) => {
-    const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
+    const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
     //TODO: get all videos based on query, sort, pagination
 
 })
 
 const publishAVideo = asyncHandler(async (req, res) => {
-    const { title, description} = req.body
+    const { title, description} = req.body;
     // TODO: get video, upload to cloudinary, create video
 
-    // if([title, description].some((field)=>field.trim()=== "")){
-    //     throw new ApiError(400, "All fields are required");
-    // }
+    if([title, description].some((field)=>field.trim()=== "")){
+        throw new ApiError(400, "All fields are required");
+    }
 
-    // const videoLocalPath = req.files
+    const videoLocalPath = req.files?.videoFile[0].path;
+
+    const thumbnailLocalPath = req.files?.thumbnail[0].path
+
+    if(!videoLocalPath && !thumbnailLocalPath){
+        throw new ApiError(400, "Video or thumbnail path is not valid");
+    }
+
+    const video = await uploadOnCloudinary(videoLocalPath);
+
+    if(!video){
+        throw new ApiError(500,"There was an error uploading video")
+    }
+
+    const thumbnail = await uploadOnCloudinary(thumbnailLocalPath);
+
+    if(!thumbnail){
+        throw new ApiError(500,"There was an error uploading thumbnail")
+    }
+
+    const newVideo = await Video.create({
+        videoFile: video.url,
+        thumbnail: thumbnail.url,
+        title,
+        description,
+        duration: video.duration,
+        owner: req.user._id
+    })
+
+    const publishedVideo = await Video.findById(newVideo._id);
+
+    if(!publishedVideo){
+        throw new ApiError(404, "Video not found")
+    }
+
+    return res.status(200)
+    .json(new ApiResponse(200, publishedVideo, "Video published successfully"))
 
 })
 
